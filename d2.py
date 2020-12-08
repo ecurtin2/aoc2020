c11994f87d40d435f7a037ff791cec81ae9362c2
evan <evanmcurtin@gmail.com>
2020-12-07 18:15:23 -0600

    cleanup

--- d2.py
+++ d2.py
@@ -23,26 +23,9 @@ def part2(x: List[str]) -> int:
     return n_valid
 
 
-def solve_part1():
-    with open(Path("inputs/d2p1.txt")) as f:
-        inputs = f.readlines()
-    return part1(inputs)
-
-
-def solve_part2():
-    with open(Path("inputs/d2p1.txt")) as f:
-        inputs = f.readlines()
-    return part2(inputs)
-
-
 def test_part1():
     assert part1(["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]) == 2
 
 
 def test_part2():
     assert part2(["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]) == 1
-
-
-if __name__ == "__main__":
-    print(solve_part1())
-    print(solve_part2())
