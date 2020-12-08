b814b603b958a00e9dc3938765fc82dcb04ef3e2
evan <evanmcurtin@gmail.com>
2020-12-08 14:11:04 -0600

    template and pyproject for pytest

---
+++ d8.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+def part1(inp: List[str]) -> int:
+    print("i am part1")
+
+
+def part2(inp: List[str]) -> int:
+    raise NotImplementedError
+
+
+def test_part1():
+    inp = """"""
+    part1(inp.splitlines())
+
+
+def test_part2():
+    inp = """"""
+    part2(inp.splitlines())
